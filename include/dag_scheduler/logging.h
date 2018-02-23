149398642aacef6b0a0e4ff3ba4de0b2edf46ebf
Matthew Hoggan <mhoggan@uber.com>
2018-02-23 11:38:27 -0700

    Doc strings finished.

--- include/dag_scheduler/logging.h
+++ include/dag_scheduler/logging.h
@@ -26,19 +26,53 @@ namespace com
 {
   namespace dag_scheduler
   {
+    //! A class used to uniqlly identify a specific log.
+    /*!
+      A generic class used to identify a specific log and its source.
+    */
     struct DLLSPEC_DAGTASKS log_tag
     {
     public:
+      //! A constructor for a \ref log_tag.
+      /*!
+        A constructor for a \ref log_tag.
+
+        \param[in] tag A string used in the logs to identify the source and
+                       to uniquly identify a log.
+      */
       explicit log_tag(const::std::string &tag);
 
+      //! A getter for the string used to represent the \ref log_tag.
+      /*!
+        A member getter function of \ref log_tag that returns the string
+        representation of the \ref log_tag.
+
+        \return A null terminated string representation of \ref log_tag.
+      */
       const char *tag() const;
 
+      //! A utility output operator.
+      /*!
+        A utility output operator for a \ref log_tag.
+
+        \param[in] os The stream to write to.
+        \param[in] tag The \ref log_tag to write to \os.
+        \return The stream written to.
+      */
       friend std::ostream &operator<<(std::ostream &os, const log_tag &tag)
       {
         os << tag.tag();
         return os;
       }
 
+      //! A utility output operator.
+      /*!
+        A utility output operator for a \ref log_tag.
+
+        \param[in] os The stream to write to.
+        \param[in] tag The \ref log_tag to write to \os.
+        \return The stream written to.
+      */
       friend std::stringstream &operator<<(std::stringstream &ss,
         const log_tag &tag)
       {
@@ -46,6 +80,17 @@ namespace com
         return ss;
       }
 
+      //! A utility operator to test for equality between \ref log_tag s.
+      /*!
+        A utility operator to test for equality between \ref log_tag s.
+
+        \param[in] lhs The \ref log_tag on the left hand side of the ==
+                       operator.
+        \param[in] rhs The \ref log_tag on the right hand side of the ==
+                       operator.
+        \return True if \ref lhs tag_ is a string equivalent to \ref rhs
+                tag_.
+      */
       friend bool operator==(const log_tag &lhs, const log_tag &rhs)
       {
         return lhs.tag() == rhs.tag();
@@ -74,6 +119,11 @@ namespace com
 {
   namespace dag_scheduler
   {
+    //! A class used to log to various sinks or sources.based on a tag.
+    /*!
+      A generic class used to log to various sinks or sources based of a
+      \ref log_tag.
+    */
     class DLLSPEC_DAGTASKS logging
     {
     private:
@@ -92,16 +142,58 @@ namespace com
       }
 
     public:
+      //! Used to create a stdout source tied to a \ref log_tag.
+      /*!
+          A static function used to create a stdout source tied to a tag.
+
+          \param[in] tag The \ref log_tag used to identify logs going to
+                         stdout stream.
+          \param[in] level A filter for a give sink and the \ref tag.
+       */
       static void add_std_cout_logger(const log_tag &tag,
         boost::log::trivial::severity_level level = DAG_SCHEDULER_INFO);
+
+      //! Used to create a stderr source tied to a \ref log_tag.
+      /*!
+          A static function used to create a stderr source tied to a tag.
+
+          \param[in] tag The \ref log_tag used to identify logs going to
+                         stderr stream.
+          \param[in] level A filter for a give sink and the \ref tag.
+       */
       static void add_std_cerr_logger(const log_tag &tag,
         boost::log::trivial::severity_level level = DAG_SCHEDULER_ERROR);
+
+      //! Used to create a stdlog source tied to a \ref log_tag.
+      /*!
+          A static function used to create a stdlog source tied to a tag.
+
+          \param[in] tag The \ref log_tag used to identify logs going to
+                         stdlog stream.
+          \param[in] level A filter for a give sink and the \ref tag.
+       */
       static void add_std_log_logger(const log_tag &tag,
         boost::log::trivial::severity_level level = DAG_SCHEDULER_TRACE);
+
+      //! Used to create a file source tied to a \ref log_tag.
+      /*!
+          A static function used to create a file source tied to a tag.
+
+          \param[in] tag The \ref log_tag used to identify logs going to
+                         a specific file stream.
+          \param[in] level A filter for a give sink and the \ref tag.
+       */
       static void add_file_logger(const log_tag &tag,
         const boost::filesystem::path &log_path,
         boost::log::trivial::severity_level level = DAG_SCHEDULER_INFO);
 
+      //! A utility function used to generate a log tag for a specific class.
+      /*!
+        A utility function used to generate a log tag for a specific class.
+
+        \param[in] instance A generic class instance.
+        \return A \ref log_tag with a string representation of \ref instance.
+      */
       template<typename T>
       static log_tag log_tag_for_this(T &instance)
       {
@@ -111,11 +203,28 @@ namespace com
       }
 
     public:
+      //! If you want to log without a empty \ref log_tag call this.
+      /*!
+        Used to set up common attributes for loggers to enable logging
+        without a \ref log_tag.
+
+        \return True if logging setup or on first call. False otherwise.
+      */
       static bool init();
 
       static boost::filesystem::path mktmpdir(
         const boost::filesystem::path &model = "%%%%-%%%%-%%%%-%%%%");
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_TRACE level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cout_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void trace(const log_tag &tag, Ts&&... args)
       {
@@ -124,6 +233,16 @@ namespace com
         write_severity_log(tag, DAG_SCHEDULER_TRACE, ss.str());
       }
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_DEBUG level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cout_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void debug(const log_tag &tag, Ts&&... args)
       {
@@ -132,6 +251,16 @@ namespace com
         write_severity_log(tag, DAG_SCHEDULER_DEBUG, ss.str());
       }
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_INFO level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cout_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void info(const log_tag &tag, Ts&&... args)
       {
@@ -140,6 +269,16 @@ namespace com
         write_severity_log(tag, DAG_SCHEDULER_INFO, ss.str());
       }
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_WARN level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cout_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void warn(const log_tag &tag, Ts&&... args)
       {
@@ -148,6 +287,16 @@ namespace com
         write_severity_log(tag, DAG_SCHEDULER_WARN, ss.str());
       }
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_ERROR level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cerr_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void error(const log_tag &tag, Ts&&... args)
       {
@@ -156,6 +305,16 @@ namespace com
         write_severity_log(tag, DAG_SCHEDULER_ERROR, ss.str());
       }
 
+      //! A key function for logging at the \ref DAG_SCHEDULER_FATAL level.
+      /*!
+        This static function logs any user defined type that can be written
+        to a \ref std::ostream and will use the tag for filtering based on
+        the call to \ref logging::add_std_cerr_logger.
+
+        \param[in] tag A \ref log_tag used in filtering.
+        \param[in] args A variadic list of args that can be written to a
+                        \ref std::ostream.
+      */
       template<typename ...Ts>
       static void fatal(const log_tag &tag, Ts&&... args)
       {
--- include/dag_scheduler/uuid.h
+++ include/dag_scheduler/uuid.h
@@ -12,26 +12,96 @@ namespace com
 {
   namespace dag_scheduler
   {
+    //! A class that represents a unique identifier.
+    /*!
+      A class that represents a directed acyclic graph (dag) and operations
+      that can be performed on it. You can add, find, check containment a
+      \ref dag_vertex. Additionally you can connect two \ref dag_vertex.
+      Every connection checks to preserve that acyclic portion of the graph.
+      If any connection would make the graph acyclic then an exception is
+      thrown and the connection is not made.
+    */
     class DLLSPEC_DAGTASKS uuid
     {
     public:
+      //! A constructor for a \ref uuid.
+      /*!
+        A constructor for a \ref uuid.
+      */
       uuid();
+
+      //! A destructor for a \ref uuid.
+      /*!
+        A destructor for a \ref uuid.
+      */
       ~uuid();
 
+      //! The move copy constructor.
+      /*!
+        The move copy constructor moves ownership of the uuid data
+        structure from this to \ref other.
+
+        \param[out] other The \ref uuid to move ownership to.
+      */
       uuid(uuid &&other);
+
+      //! Move assignment operator.
+      /*!
+        The move assignment operator moves ownership of the uuid data
+        structure from this to \ref other.
+      */
       uuid &operator=(uuid &&rhs);
 
+      //! Clones this object into an identical uuid
+      /*!
+        A member function of \ref uuid that makes an identical copy of this
+        and returns it.
+
+        \return A \ref uuid that is an identical copy of this.
+      */
       uuid clone();
 
+      //! In the case a move of a \ref uuid this function will return false.
+      /*!
+        A function to be used to check if \ref this was moved to another
+        \ref uuid.
+
+        \return True if \ref this was moved, false otherwise.
+      */
       bool is_initialized() const;
+
+      //! Get a string representation of the \ref uuid.
+      /*!
+        A member fucntion of \ref uuid that returns a string representation.
+
+        \return A string representation of the \ref uuid.
+      */
       std::string as_string() const;
 
+      //! A utility function for streaming out a uuid.
+      /*!
+          A utility function for streaming out a uuid.
+
+          \param[in] out The stream to write \ref u to.
+          \param[in] u The \ref uuid to write out to \ref out.
+          \return The \ref out param written to.
+      */
       friend std::ostream &operator<<(std::ostream &out, const uuid &u)
       {
         out << u.as_string();
         return out;
       }
 
+      //! A utility function for checking equality of two \ref uuid s.
+      /*!
+          A utility function for checking equality of two \ref uuid.
+
+          \param[in] lhs The \ref uuid to the left hand side of the == op.
+          \param[in] rhs The \ref uuid to the right hand side of the == op.
+          \return True if \ref lhs and \ref rhs are not initialized, or
+                  if the string representation of \ref rhs equals that of
+                  \ref rhs. False otherwise.
+      */
       friend bool operator==(const uuid &lhs, const uuid &rhs)
       {
         bool ret = true;
@@ -48,6 +118,16 @@ namespace com
         return ret;
       }
 
+      //! A utility function for checking inequality of two \ref uuid s.
+      /*!
+          A utility function for checking inequality of two \ref uuid.
+
+          \param[in] lhs The \ref uuid to the left hand side of the == op.
+          \param[in] rhs The \ref uuid to the right hand side of the == op.
+          \return False if \ref lhs and \ref rhs are not initialized, or
+                  if the string representation of \ref rhs equals that of
+                  \ref rhs. True otherwise.
+      */
       friend bool operator!=(const uuid &lhs, const uuid &rhs)
       {
         return !(lhs == rhs);
