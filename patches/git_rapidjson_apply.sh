c07bfa9589655ea8289c9b1c8ad2f82e018cdfd9
Matthew Hoggan <matthew.hoggan@climate.com>
2022-02-14 10:17:13 -0600

    Fixes issues with build in gitlab.

--- patches/git_rapidjson_apply.sh
+++ patches/git_rapidjson_apply.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
+set -e
 
 pushd `dirname "${0}"`
 BASEDIR=$(pwd)
 popd > /dev/null
 
-BUILD_DIRS=($(find "${BASEDIR}/.." -maxdepth 1 -name '*build*' -type d))
-printf 'BUILD_DIRS = %s\n' "${BUILD_DIRS[@]}"
-
-s2_geometry_patch() {
+apply_patch() {
   PATCH_FILE=rapidjson.patch
   pushd "${1}/rapidjson-src"
+  echo "Going to apply patch in ${1}/rapidjson-src..."
   git apply --ignore-space-change --ignore-whitespace "${BASEDIR}/${PATCH_FILE}"
   popd
 }
 
-for dir in "${BUILD_DIRS[@]}";
-do
-  echo "Running patch in ${dir}."
-  s2_geometry_patch "${dir}"
-done
+apply_patch ${1}
