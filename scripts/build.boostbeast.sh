4f6d2baad80fe22e2fc87f38eaa2a748cdeded50
Matthew Hoggan <matthew.hoggan@climate.com>
2022-06-08 16:10:57 -0500

    Updates to build to include env variable.

--- scripts/build.boostbeast.sh
+++ scripts/build.boostbeast.sh
@@ -5,6 +5,8 @@ BUILD_DIR=${1}
 MAKE_CMD=$(which make)
 echo "Going to build boostbeast in ${BUILD_DIR} with ${MAKE_CMD}..."
 
+PYTHON_INCLUDES="/Library/Frameworks/Python.framework/Versions/3.8"
+PYTHON_INCLUDES="${PYTHON_INCLUDES}/include/python3.8"
 cd ${BUILD_DIR}/boostbeast-src && \
   export CC="/usr/bin/clang" && \
   export CXX="/usr/bin/clang++" && \
@@ -18,4 +20,6 @@ cd ${BUILD_DIR}/boostbeast-src && \
   export OPENSSL_ROOTDIR="${BUILD_DIR}/openssl" && \
   export OPENSSL_INCLUDE_DIR="${BUILD_DIR}/openssl/include" && \
   export OPENSSL_LIBRARIES="${BUILD_DIR}/openssl/lib" && \
+  export C_INCLUDE_PATH="${PYTHON_INCLUDES}:${C_INCLUDE_PATH}" && \
+  export CPLUS_INCLUDE_PATH="${PYTHON_INCLUDES}:${CPLUS_INCLUDE_PATH}" && \
   ./b2 -j2 cxxstd=11
